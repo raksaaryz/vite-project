bc4cd1f8c6783973cbc6286809ae119f8dec0a90
raksaaryz <raksaarz@gmail.com>
2024-05-31 18:44:27 +0700

    TUGAS REACT NEBULA

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import './index.css'
 
 function App() {
   const data ={
-    foto : "../fto.jpg",
-    name : "Botak Gacor",
-    born : "25 Mei 2004",
+    foto : "./foto bagus.jpg",
+    name : "Kang Joki Handal",
+    born : "10 Januari 2004",
     age : "20 Tahun",
-    hobby : ["bola","raket","renang"]
+    hobby : ["Joki Jurnal","Joki Makalah","Joki UAS"]
   }
 
   return (
@@ -18,7 +18,7 @@ function App() {
         <div className='content'>
           <h1>Biodata</h1>
           <img src={data.foto} alt="" />
-          <h3 className='coret'>&#128526;{data.name}&#128526;</h3>
+          <h3>{data.name}</h3>
           <p className='isi1'>{data.born}</p>
           <p className='isi2'>{data.age}</p>
           <ul className='hobby'>
